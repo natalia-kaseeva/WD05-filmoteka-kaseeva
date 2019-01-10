a8ad29e0a47a7ab13a7c727feec2e48113de3281
Natalia <nat.kaseeva@gmail.com>
2019-01-10 18:32:59 +0400

    filmoteka-4,edit.php was fixed

--- login.php
+++ login.php
@@ -1,8 +1,7 @@
 <?php 
-
 require('config.php');
-require('database.php');
-$link = db_connect();
+//require('database.php');
+//$link = db_connect();
 //require('models/users.php');
 require('functions/login-functions.php');
 
@@ -12,21 +11,17 @@ if ( isset($_POST['enter']) ) {
 	$userName=$_POST['user-name'];
 	$userCity=$_POST['user-city'];
 	$expire=time()+60*60*24*30;
-
 	if ( $_POST['login'] == $userLogin ) {
 		if ( $_POST['password'] == $userPassword ) {
 			//session_start();
 			$_SESSION['user'] = 'admin';
-
 			setcookie('user-name',$userName,$expire);
 			setcookie('user-city',$userCity,$expire);
 			header('Location: ' . HOST . 'index.php');
 		}
 	}
 }
-
 include('views/head.tpl');
 include('views/login.tpl');
 include('views/footer.tpl');
-
  ?>
